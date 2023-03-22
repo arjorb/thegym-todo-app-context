b3e14cbda4ec0fef4fae92bdb9093985a5df23cc
John U <arjorb@gmail.com>
2023-03-22 20:46:04 +0200

    creating components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <h1 className='text-3xl text-[#019875] font-bold flex justify-center'>Hello context API!</h1>;
-}
+import Todo from './components/Todo';
+
+const App = () => {
+  return <Todo />;
+};
 
 export default App;
